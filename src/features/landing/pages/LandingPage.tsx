8385de519e5fe47e09c7ef8a2c6db915ea8c3d5d
balntvdd11 <baltazarnatividad012@gmail.com>
2026-08-16 23:36:27 +0800

    style(landing): increase size of white blob

--- src/features/landing/pages/LandingPage.tsx
+++ src/features/landing/pages/LandingPage.tsx
@@ -133,7 +133,7 @@ export default function LandingPage({ onLoginClick, onSplitComplete }: Props) {
             layout
             animate={blobControls}
             initial={{ backgroundColor: stage === "idle" ? "#ffffff" : "#082f6e" }}
-            className="absolute lg:translate-y-0 w-[200vw] h-[200vw] lg:w-[max(990px,120vh)] lg:h-[max(990px,120vh)] rounded-full"
+            className="absolute lg:translate-y-0 w-[220vw] h-[220vw] lg:w-[max(1100px,135vh)] lg:h-[max(1100px,135vh)] rounded-full"
             style={{
               opacity: 1,
               top: isDesktop ? "-15%" : "auto",
